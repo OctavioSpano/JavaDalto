6f31b90658db0d480ab157c3e0172cc3d0c00578
OctavioSpano <101335584+OctavioSpano@users.noreply.github.com>
2023-05-18 13:57:53 -0300

    arrays

---
+++ ArraysDalto.js
@@ -0,0 +1,15 @@
+//Arrays 
+
+// let frutas = ["manzana", "banana", "frutilla"]
+// document.write(frutas[2])
+
+//Arrays Asociativos
+
+let infoYo = {
+    nombre: "Octavio",
+    apellido: "Spano",
+    curso: "5°",
+    division: "B"
+};
+
+document.write(infoYo["division"])
